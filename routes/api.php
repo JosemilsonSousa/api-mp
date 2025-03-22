8242c1008c94471e151b6d34841f88feef61e443
Josemilson Sousa <josemilsonsousa@yahoo.com.br>
2025-03-22 20:49:03 -0300

    First Api

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,10 @@ Route::prefix('api/v1')->group(function () {
 	    Route::get('/users', 		'index');
 	    Route::get('/user/{user}', 	'show');
 	    Route::post('/user', 		'store');
-	    Route::put('/user/{user}', 	'update');
+	    Route::post('/user/{user}', 'update');
 	    Route::delete('/user/{user}', 'destroy');
 	});
-	
+
 	Route::get('/csrf-token', function () {
 	    return response()->json(['csrf_token' => csrf_token()]);
 	});

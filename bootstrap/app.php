e37db4d4b6705a43c2d26ec93b770c37355383a7
Josemilson Sousa <josemilsonsousa@yahoo.com.br>
2025-03-22 20:32:10 -0300

    First Api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,8 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->validateCsrfTokens(except: [
-            'https://apimp.inforaison.com.br/api/v1/user',
-            'https://apimp.inforaison.com.br/api/v1/user/{user}',
+            'https://apimp.inforaison.com.br/api/v1/*',
         ]);
         //
     })

6b47d4b91842426e843089386662a54a3c3cefe4
Josemilson Sousa <josemilsonsousa@yahoo.com.br>
2025-03-22 12:56:26 -0300

    First Api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,42 +8,33 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        // $users = User::all();
         return response()->json(['users' => User::all()]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $user = new User;
+
+        $user->name     = $request->name;
+        $user->email    = $request->email;
+        $user->doc      = $request->doc;
+        $user->save();
+
+        return response()->json(['user' => $user]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(User $user)
     {
         return response()->json(['user' => $user]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, User $user)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
         //
